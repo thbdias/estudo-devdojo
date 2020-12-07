f3422e12455954aee8291fd7c56ef31cb6db2667
thbd <thiagobalbino91@gmail.com>
2020-12-07 20:58:23 -0300

    tratamento erro 3 - aula 13

--- src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,6 +48,7 @@ public class StudentEndPoint {
     }	
 	
 	@PostMapping()
+	@Transactional(rollbackFor = Exception.class)
 	public ResponseEntity<?> save(@RequestBody Student student) {		
 		return new ResponseEntity<>(studentDAO.save(student),HttpStatus.CREATED);
 	}
