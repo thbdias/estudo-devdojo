544e37086ddc6fa0aef36ff6c260be8dcefdcba0
thbd <thiagobalbino91@gmail.com>
2020-12-08 19:25:00 -0300

    cliente spring com rest template pt 03 put delete - video 25

--- src/main/java/br/com/devdojo/javaclient/JavaClientDAO.java
+++ src/main/java/br/com/devdojo/javaclient/JavaClientDAO.java
@@ -1,58 +1,70 @@
-//package br.com.devdojo.javaclient;
-//
-//import br.com.devdojo.handler.RestResponseExceptionHandler;
-//import br.com.devdojo.model.PageableResponse;
-//import br.com.devdojo.model.Student;
-//import org.springframework.boot.web.client.RestTemplateBuilder;
-//import org.springframework.core.ParameterizedTypeReference;
-//import org.springframework.http.*;
-//import org.springframework.web.client.RestTemplate;
-//
-//import java.util.List;
-//
-///**
-// * @author William Suane for DevDojo on 7/6/17.
-// */
-//public class JavaClientDAO {
-//    private RestTemplate restTemplate = new RestTemplateBuilder()
-//            .rootUri("http://localhost:8080/v1/protected/students")
-//            .basicAuthorization("toyo", "devdojo")
-//            .errorHandler(new RestResponseExceptionHandler())
-//            .build();
-//    private RestTemplate restTemplateAdmin = new RestTemplateBuilder()
-//            .rootUri("http://localhost:8080/v1/admin/students")
-//            .basicAuthorization("toyo", "devdojo")
-//            .errorHandler(new RestResponseExceptionHandler())
-//            .build();
-//
-//    public Student findById(long id) {
-//        return restTemplate.getForObject("/{id}", Student.class, id);
-////        ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 1);
-//    }
-//
-//    public List<Student> listAll() {
+package br.com.devdojo.javaclient;
+
+import java.util.List;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.devdojo.handler.RestResponseExceptionHandler;
+import br.com.devdojo.model.Student;
+
+/**
+ * @author William Suane for DevDojo on 7/6/17.
+ */
+public class JavaClientDAO {
+    private RestTemplate restTemplate = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/protected/students")
+            .basicAuthentication("abc", "devdojo")     
+            .errorHandler(new RestResponseExceptionHandler())
+            .build();
+    
+    private RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/admin/students")
+            .basicAuthentication("admin", "devdojo")
+            .errorHandler(new RestResponseExceptionHandler())
+            .build();
+
+    public Student findById(long id) {
+        return restTemplate.getForObject("/{id}", Student.class, id);
+//        ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, id);
+    }
+
+    public List<Student> listAll() {
+    	//deu erro com pageable
 //        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
 //                new ParameterizedTypeReference<PageableResponse<Student>>() {
 //                });
-//        return exchange.getBody().getContent();
-//    }
-//
-//    public Student save(Student student) {
-//        ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/",
-//                HttpMethod.POST, new HttpEntity<>(student, createJSONHeader()), Student.class);
-//        return exchangePost.getBody();
-//    }
-//
-//    public void update(Student student){
-//        restTemplateAdmin.put("/",student);
-//    }
-//    public void delete(long id){
-//        restTemplateAdmin.delete("/{id}",id);
-//    }
-//
-//    private static HttpHeaders createJSONHeader() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        return headers;
-//    }
-//}
+//        return exchange.getBody().getContent();    	
+
+    	return restTemplate
+    				.exchange("/list", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {})
+    				.getBody();
+//    	Student[] students = restTemplate.getForObject("/list", Student[].class);
+    }
+
+    public Student save(Student student) {        
+    	ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/", HttpMethod.POST, new HttpEntity<>(student, createJSONHeader()), Student.class);
+//    	Student studentPostForObject = restTemplateAdmin.postForObject("/", student, Student.class);
+//    	ResponseEntity<Student> studentPostForEntity = restTemplateAdmin.postForEntity("/", student, Student.class);    	
+        return exchangePost.getBody();
+    }
+
+    public void update(Student student){
+        restTemplateAdmin.put("/",student);
+    }
+    public void delete(long id){
+        restTemplateAdmin.delete("/{id}",id);
+    }
+
+    private static HttpHeaders createJSONHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}
--- src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
+++ src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
@@ -20,65 +20,24 @@ import br.com.devdojo.model.Student;
  */
 public class JavaSpringClientTest {
     public static void main(String[] args) {
+    	
+    	JavaClientDAO javaClientDAO = new JavaClientDAO();
+    	
+    	javaClientDAO.findById(10);
+    	
+    	javaClientDAO.listAll();
 
-    	RestTemplate restTemplate = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v1/protected/students")
-                .basicAuthentication("abc", "devdojo")                
-                .build();
-    	
-    	RestTemplate restTemplateAdmin = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v1/admin/students")
-                .basicAuthentication("admin", "devdojo")                
-                .build();
-    	
-    	//get by id com getForObject
-    	Student student = restTemplate.getForObject("/{id}", Student.class, 10);
-    	System.out.println(student);
-    	
-    	//get by id com getForEntity
-    	ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 10);
-    	System.out.println(forEntity.getBody());
-    	
-    	//get all com getForObject
-    	Student[] students = restTemplate.getForObject("/list", Student[].class);
-    	System.out.println(Arrays.toString(students));
-    	
-    	//get all com exchange
-    	ResponseEntity<List<Student>> exchange = restTemplate.exchange("/list", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {});
-    	System.out.println(exchange.getBody());
-    	    	
-    	//post com exchange
     	Student studentPost = new Student();
     	studentPost.setName("John Wick");
     	studentPost.setEmail("john@pencil.com");
-    	ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/", HttpMethod.POST, new HttpEntity<>(studentPost, createJSONHeader()), Student.class);
-    	System.out.println(exchangePost);
     	
-    	//post com getForObject
-    	Student studentPostForObject = restTemplateAdmin.postForObject("/", studentPost, Student.class);
-    	System.out.println(studentPostForObject);
+    	javaClientDAO.save(studentPost);
     	
-    	//post com getForEntity
-    	ResponseEntity<Student> studentPostForEntity = restTemplateAdmin.postForEntity("/", studentPost, Student.class);
-    	System.out.println(studentPostForEntity);
+    	studentPost.setId(10L);
+    	studentPost.setName("nome teste 2");
+    	javaClientDAO.update(studentPost);
     	
-//        Student studentPost = new Student();
-//        studentPost.setName("John Wick 2");
-//        studentPost.setEmail("john@pencil.com");
-//        studentPost.setId(29L);
-//        JavaClientDAO dao = new JavaClientDAO();
-//        System.out.println(dao.findById(111));
-//        List<Student> students = dao.listAll();
-//        System.out.println(students);
-//        System.out.println(dao.save(studentPost));
-//        dao.update(studentPost);
-//        dao.delete(29);
-    }
-    
-    private static HttpHeaders createJSONHeader() {
-    	HttpHeaders headers = new HttpHeaders();
-    	headers.setContentType(MediaType.APPLICATION_JSON);
-    	return headers;
-    }
-
+    	javaClientDAO.delete(10L);
+    	
+    }    
 }
