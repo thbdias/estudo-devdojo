8e92b17e19cd1a5a0cc71b995d52b14e23cd86b8
thbd <thiagobalbino91@gmail.com>
2020-12-08 18:58:59 -0300

    cliente spring com rest template pt 03 post - (deu errado o video anterior de pageable) - video 24

--- src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
+++ src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,14 +22,21 @@ public class JavaSpringClientTest {
     public static void main(String[] args) {
 
     	RestTemplate restTemplate = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v1/protected/students")                
+                .rootUri("http://localhost:8080/v1/protected/students")
                 .basicAuthentication("abc", "devdojo")                
                 .build();
     	
-    	//get by id
-    	Student student = restTemplate.getForObject("/{id}", Student.class, 5);
-    	ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 5);    	
+    	RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+                .rootUri("http://localhost:8080/v1/admin/students")
+                .basicAuthentication("admin", "devdojo")                
+                .build();
+    	
+    	//get by id com getForObject
+    	Student student = restTemplate.getForObject("/{id}", Student.class, 10);
     	System.out.println(student);
+    	
+    	//get by id com getForEntity
+    	ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 10);
     	System.out.println(forEntity.getBody());
     	
     	//get all com getForObject
@@ -36,6 +46,21 @@ public class JavaSpringClientTest {
     	//get all com exchange
     	ResponseEntity<List<Student>> exchange = restTemplate.exchange("/list", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {});
     	System.out.println(exchange.getBody());
+    	    	
+    	//post com exchange
+    	Student studentPost = new Student();
+    	studentPost.setName("John Wick");
+    	studentPost.setEmail("john@pencil.com");
+    	ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/", HttpMethod.POST, new HttpEntity<>(studentPost, createJSONHeader()), Student.class);
+    	System.out.println(exchangePost);
+    	
+    	//post com getForObject
+    	Student studentPostForObject = restTemplateAdmin.postForObject("/", studentPost, Student.class);
+    	System.out.println(studentPostForObject);
+    	
+    	//post com getForEntity
+    	ResponseEntity<Student> studentPostForEntity = restTemplateAdmin.postForEntity("/", studentPost, Student.class);
+    	System.out.println(studentPostForEntity);
     	
 //        Student studentPost = new Student();
 //        studentPost.setName("John Wick 2");
@@ -48,7 +73,12 @@ public class JavaSpringClientTest {
 //        System.out.println(dao.save(studentPost));
 //        dao.update(studentPost);
 //        dao.delete(29);
-
+    }
+    
+    private static HttpHeaders createJSONHeader() {
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.setContentType(MediaType.APPLICATION_JSON);
+    	return headers;
     }
 
 }
